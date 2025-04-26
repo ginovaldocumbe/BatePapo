ef15418293f0e078137c45cb75b9613712efdb0d
ginovaldocumbe <92864360+ginovaldocumbe@users.noreply.github.com>
2025-04-26 18:19:57 +0200

    feat: Implement follow/unfollow functionality and update user card display

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class FollowerController extends Controller
 {
     //
+    public function follow(User $user)
+    {
+        $follower = auth()->user();
+        $follower->followings()->attach($user);
+
+        return redirect()->route('users.show', $user)->with('success', 'You are now following ' . $user->name);
+    }
+    public function unfollow(User $user)
+    {
+        $follower = auth()->user();
+        $follower->followings()->detach($user);
+
+        return redirect()->route('users.show', $user)->with('success', 'You are no longer following ' . $user->name);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -54,6 +54,17 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function followings(){
+        return $this->belongsToMany(User::class, 'follower_user', 'follower_id', 'user_id')->withTimestamps();
+    }
+    public function followers(){
+        return $this->belongsToMany(User::class, 'follower_user', 'user_id', 'follower_id')->withTimestamps();
+    }
+    public function isFollowing($user)
+    {
+        return $this->followings()->where('user_id', $user->id)->exists();
+    }
     public function getImageUrl()
     {
         if ($this->image) {
